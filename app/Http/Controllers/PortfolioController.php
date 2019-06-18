487ee558d72056842aa8aa38563600c7d22ed77a
Juan Manuel Oviedo Carrasco <juanmanuel85@gmail.com>
2019-06-18 17:41:29 -0400

    leccion 14

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -7,19 +7,84 @@ use Illuminate\Http\Request;
 class PortfolioController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Display a listing of the Proyecto.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $portfolio = [
+            ['title' => 'Proyecto # 1 '],
+            ['title' => 'Proyecto # 2 '],
+            ['title' => 'Proyecto # 3 '],
+            ['title' => 'Proyecto # 4 '],
+        ];
+        return view('portfolio', compact('portfolio'));
+    }
+
+    /**
+     * Show the form for creating a new Proyecto.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created Proyecto in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function store(Request $request)
     {
-            $portfolio = [
-        ['title' => 'Proyecto # 1'],
-        ['title' => 'Proyecto # 2'],
-        ['title' => 'Proyecto # 3'],
-        ['title' => 'Proyecto # 4'],
-];
-      return view('portfolio', compact('portfolio'));
+        //
+    }
+
+    /**
+     * Display the specified Proyecto.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified Proyecto.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified Proyecto in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified Proyecto from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@
 Route::view('/' , 'home')->name('home');
 Route::view('/about' , 'about')->name('about');
 /*Route::view('/portfolio' , 'portfolio', compact('portfolio'))->name('portfolio');*/
-Route::get('/portfolio','PortfolioController')->name('portfolio');
+Route::get('/portfolio','PortfolioController@index')->name('portfolio');
 Route::view('/contact' , 'contact')->name('contact');
+/*Route::resource('portflio', ('PortfolioController'));*/
