dddbe38f4e545e089bf6f5a5bc6c4918b8e23995
Juan Manuel Oviedo Carrasco <juanmanuel85@gmail.com>
2019-06-26 12:36:29 -0400

    leccion 15

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ Route::view('/about' , 'about')->name('about');
 /*Route::view('/portfolio' , 'portfolio', compact('portfolio'))->name('portfolio');*/
 Route::get('/portfolio','PortfolioController@index')->name('portfolio');
 Route::view('/contact' , 'contact')->name('contact');
-/*Route::resource('portflio', ('PortfolioController'));*/
+/*con la proxima linea se generan todos los métodos  del controlador*/
+/*Route::resource('proyectos', ('PortfolioController'));*/
