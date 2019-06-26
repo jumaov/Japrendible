508c2b1270754f1a955e3b17fa50e1772ce94012
Juan Manuel Oviedo Carrasco <juanmanuel85@gmail.com>
2019-06-26 15:44:57 -0400

    leccion 16

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,12 @@
 @section('title' , 'Contac')
 @section('content')
 	<h1>Contact</h1>
+	<form method="POST" action="{{ route('contact') }}">
+		@csrf
+		<input name="name" placeholder="Nombre..."  value="Juan"><br>
+		<input type="email" name="email" placeholder="Email..." value="juan@hotmail.com"><br>
+		<input name="subject" placeholder="Asunto..." value="Asunto de Prueba"><br>
+		<textarea name="content" placeholder="Mensaje..." >Mensaje de Prueba</textarea><br>
+		<button>Enviar</button>
+	</form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ Route::view('/about' , 'about')->name('about');
 /*Route::view('/portfolio' , 'portfolio', compact('portfolio'))->name('portfolio');*/
 Route::get('/portfolio','PortfolioController@index')->name('portfolio');
 Route::view('/contact' , 'contact')->name('contact');
+Route::post('contact', 'MessagesController@store');
 /*con la proxima linea se generan todos los métodos  del controlador*/
 /*Route::resource('proyectos', ('PortfolioController'));*/
